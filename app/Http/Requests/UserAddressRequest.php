9be9ccbfa294924b4f4861970cc887faaad33127
791667400@qq.com <791667400@qq.com>
2019-03-21 20:01:56 +0800

    新增收货地址

--- app/Http/Requests/UserAddressRequest.php
+++ app/Http/Requests/UserAddressRequest.php
@@ -7,13 +7,13 @@ class UserAddressRequest extends Request
     public function rules()
     {
         return [
-            'province'      => '省',
-            'city'          => '城市',
-            'district'      => '地区',
-            'address'       => '详细地址',
-            'zip'           => '邮编',
-            'contact_name'  => '姓名',
-            'contact_phone' => '电话',
+            'province'      => 'required',
+            'city'          => 'required',
+            'district'      => 'required',
+            'address'       => 'required',
+            'zip'           => 'required',
+            'contact_name'  => 'required',
+            'contact_phone' => 'required',
         ];
     }
 }
