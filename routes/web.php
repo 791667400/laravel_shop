4e481917ceda26c7b45a180e1c78c79eae2ee118
huqw <huqw@huqwdeMacBook-Air.local>
2019-03-26 16:30:50 +0800

    修改和删除收货地址

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,10 @@ Route::group(['middleware' => 'auth'], function() {
         });
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     });
     // 结束
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
